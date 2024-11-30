7a1753b914405fe564192a212d05b82095117925
mango2309 <sebastian.largo@udla.edu.ec>
2024-11-30 11:44:23 -0500

    Se editó el About Page para el punto extra.

--- TallerApuntesSebastianLargo/Models/About.cs
+++ TallerApuntesSebastianLargo/Models/About.cs
@@ -8,9 +8,10 @@ namespace TallerApuntesSebastianLargo.Models
 {
     internal class About
     {
-        public string Title => AppInfo.Name;
-        public string Version => AppInfo.VersionString;
-        public string MoreInfoUrl => "https://aka.ms/maui";
-        public string Message => "This app is written in XAML and C# with .NET MAUI.";
+        public string Title { get; set; } = "Sobre mí";
+        public string Version { get; set; } = "1.0";
+        public string Name { get; set; } = "Sebastián Largo";
+        public string Description { get; set; } = "Soy estudiante de software que le gustan las hamburguesas y el mango.";
+        public string Icon { get; set; } = "icon_personal1.jpg"; 
     }
 }
--- TallerApuntesSebastianLargo/Views/AboutPage.xaml.cs
+++ TallerApuntesSebastianLargo/Views/AboutPage.xaml.cs
@@ -8,10 +8,6 @@ public partial class AboutPage : ContentPage
 	}
     private async void LearnMore_Clicked(object sender, EventArgs e)
     {
-        if (BindingContext is Models.About about)
-        {
-            // Navigate to the specified URL in the system browser.
-            await Launcher.Default.OpenAsync(about.MoreInfoUrl);
-        }
+        await DisplayAlert("Información", "Gracias por visitar este increible proyecto.", "Cerrar");
     }
 }
